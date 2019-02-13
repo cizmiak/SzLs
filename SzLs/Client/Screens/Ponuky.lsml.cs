081725a633522d9abaec02c00114694f98beed6e
Matej Cizmarik <Matej.Cizmarik@2Ring.com>
2019-02-14 00:15:20 +0100

    offers scrren

---
+++ SzLs/Client/Screens/Ponuky.lsml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LightSwitchApplication
+{
+	public partial class Ponuky
+	{
+		partial void Odosli_CanExecute(ref bool result)
+		{
+			result = this.Offers.SelectedItem != null;
+		}
+
+		partial void Odosli_Execute()
+		{
+			this.Offers.SelectedItem.DatumOdoslania = DateTime.Now;
+			this.Save();
+		}
+	}
+}
---
+++ SzLs/Server/DataSources/SpravaZmluvData/Ponuka.lsml.cs
@@ -0,0 +1,14 @@
+﻿namespace LightSwitchApplication
+{
+	public partial class Ponuka
+	{
+		partial void EmailSablona_Changed()
+		{
+			if (this.EmailSablona != null)
+			{
+				this.Subject = this.EmailSablona.Subject;
+				this.TeloSpravy = this.EmailSablona.TeloSpravy;
+			}
+		}
+	}
+}
